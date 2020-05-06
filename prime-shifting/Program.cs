1050f3a2bfa31a50233004e9e22fd407b0f0038a
unknown <macjay314@gmail.com>
2020-05-06 11:05:44 -0700

    Create basic UI that will ask the user for a number, check if it's an int, then respond accordingly

---
+++ prime-shifting/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimeShifting
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello! I bet I can count all of the prime numbers under any given number. Let's test that. Enter a number please.");
+      int input = Console.ReadLine();
+      if (int.TryParse(foo, out int number)) {
+        //Call method that returns list of numbers. Param (number)
+        //Call method that returns list of prime numbers. Param (list)
+      } 
+      else 
+      {
+        Console.WriteLine("Unfortunately I don't believe that's a number I can test. Please only enter whole numbers.");
+      }
+    }
+  }
+}
