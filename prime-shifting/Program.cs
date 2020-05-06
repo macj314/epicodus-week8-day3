040c8591793b26c1e354b644d4a72f9209d5b129
unknown <macjay314@gmail.com>
2020-05-06 13:52:43 -0700

    Create a Prime object to call functions, and a list of ints to manipulate. Print out list

--- prime-shifting/Program.cs
+++ prime-shifting/Program.cs
@@ -8,9 +8,14 @@ namespace PrimeShifting
     public static void Main()
     {
       Console.WriteLine("Hello! I bet I can count all of the prime numbers under any given number. Let's test that. Enter a number please.");
-      int input = Console.ReadLine();
-      if (int.TryParse(foo, out int number)) {
-        //Call method that returns list of numbers. Param (number)
+      string inputString = Console.ReadLine();
+      if (int.TryParse(inputString, out int inputInt)) {
+        Prime maxInt = new Prime(inputInt);
+        List<int> listInt = maxInt.CreateList();
+        foreach (int i in listInt)
+        {
+          Console.WriteLine(i);
+        }
         //Call method that returns list of prime numbers. Param (list)
       } 
       else 
