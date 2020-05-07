5196f8890195488ceea697a7828fba8a2817a210
unknown <macjay314@gmail.com>
2020-05-07 10:32:58 -0700

    Finish debugging. CreateList() now sets a bool (flag) that can be used to flag if a number is prime. Add any numbers that are flagged to listNumbers and return it

--- prime-shifting/Prime.cs
+++ prime-shifting/Prime.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Linq;
 using System.Collections.Generic;
-
 
 namespace PrimeShifting
 {
+
   public class Prime
   {
     private int _input;
@@ -15,38 +13,24 @@ namespace PrimeShifting
 
    public List<int> CreateList()
    {
-     List<int> listNumbers = new List<int>();
-     for (int i = 2; i <= _input; i++)
-     {
-       for (int j = 2; j * j <= i; j++)
-       {
-         Console.WriteLine("i=" + i);
-         Console.WriteLine("j=" + j);
-         Console.WriteLine(i%j);
-         if (i % j != 0)
-         {
-           listNumbers.Add(i);
-         }
-       }
-     }
+    List<int> listNumbers = new List<int>();
+    for (int i = 2; i <= _input; i++)
+    {
+      bool flag = true;
+      for (int j = 2; j * j <= i; j++)
+      {
+        if (i % j == 0)
+        {
+          flag = false;
+          break;
+        }
+      }
+      if (flag)
+      {
+        listNumbers.Add(i);
+      }
+    }
      return listNumbers;
    }
-
-  //  public List<int> FindPrimeNums(List<int> listInt)
-  //  {
-  //    int max = listInt.Last();
-  //    for (int prime = 2; prime <= max; prime++)
-  //    {
-  //      for (int i = 2; i * i <= prime; i++)
-  //      {
-  //        if (prime % i == 0)
-  //        {
-  //          listInt.Remove(prime);
-  //        }
-  //      }
-  //    }
-  //    return listInt;
-  //  }
-  // }
-}
+  }
 }
